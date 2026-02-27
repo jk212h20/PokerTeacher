be2ab8030c655e17ed7651ffa7d9a7f0cc7adb09
Nick <nick@Nicks-MacBook-Pro.local>
2026-02-26 20:48:58 -0500

    Add Node.js static server for Railway deployment

---
+++ server.js
@@ -0,0 +1,47 @@
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
+
+const PORT = process.env.PORT || 3000;
+
+const MIME = {
+  '.html': 'text/html',
+  '.css':  'text/css',
+  '.js':   'application/javascript',
+  '.json': 'application/json',
+  '.png':  'image/png',
+  '.jpg':  'image/jpeg',
+  '.svg':  'image/svg+xml',
+  '.ico':  'image/x-icon',
+  '.woff2':'font/woff2',
+  '.woff': 'font/woff',
+};
+
+http.createServer((req, res) => {
+  let urlPath = req.url.split('?')[0];
+  if (urlPath === '/') urlPath = '/index.html';
+
+  const filePath = path.join(__dirname, urlPath);
+  const ext = path.extname(filePath);
+  const contentType = MIME[ext] || 'application/octet-stream';
+
+  fs.readFile(filePath, (err, data) => {
+    if (err) {
+      // Serve index.html for any unknown path (SPA fallback)
+      fs.readFile(path.join(__dirname, 'index.html'), (err2, data2) => {
+        if (err2) {
+          res.writeHead(500);
+          res.end('Server error');
+        } else {
+          res.writeHead(200, { 'Content-Type': 'text/html' });
+          res.end(data2);
+        }
+      });
+    } else {
+      res.writeHead(200, { 'Content-Type': contentType });
+      res.end(data);
+    }
+  });
+}).listen(PORT, () => {
+  console.log(`PokerTeacher running on port ${PORT}`);
+});
