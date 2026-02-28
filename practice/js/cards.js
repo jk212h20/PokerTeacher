0b0d798d1985281715f6bc7cacef115ae06dad79
Nick <nick@Nicks-MacBook-Pro.local>
2026-02-28 11:15:27 -0500

    Fix cards: T instead of 10, consistent aspect-ratio 5:7 on all cards

--- practice/js/cards.js
+++ practice/js/cards.js
@@ -16,7 +16,7 @@ const RANK_VALUES = {
 
 const RANK_DISPLAY = {
     '2': '2', '3': '3', '4': '4', '5': '5', '6': '6', '7': '7', '8': '8', '9': '9',
-    'T': '10', 'J': 'J', 'Q': 'Q', 'K': 'K', 'A': 'A'
+    'T': 'T', 'J': 'J', 'Q': 'Q', 'K': 'K', 'A': 'A'
 };
 
 // Create a card object
